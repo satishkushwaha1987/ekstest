74cf0d7dbd4e2cbc1ba13b497fca0dbd887e75ba
satishkushwaha1987 <satishkushwaha1987@gmail.com>
2024-06-24 14:14:13 +0530

    add new branch logs & remove crediantials from config file

--- config.py
+++ config.py
@@ -4,7 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'LD1OHNrAoIsdusrimjV7aoKhn/ZqdEOTtj8RDm+5'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://logs:Kushwaha1987@localhost/logs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    AWS_ACCESS_KEY_ID = os.environ.get('AKIA47CRY4FXQJTGZYHJ')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('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')
-    AWS_S3_BUCKET = os.environ.get('s3tochlogtest')
 
